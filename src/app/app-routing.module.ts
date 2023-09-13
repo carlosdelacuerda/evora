89df02774af9a9d3a70121e95f597b379eebdbc2
carlosdelacuerda <carlosdelacuerda@gmail.com>
2023-09-13 17:02:34 +0200

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list'
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./pages/list/list.module').then(m => m.ListModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'list'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
