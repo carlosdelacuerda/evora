59db3d769d169333da4eba5e7f016ecb4573e26c
carlosdelacuerda <carlosdelacuerda@gmail.com>
2023-09-20 19:09:11 +0200

    book service

--- src/app/components/table/book/book.component.ts
+++ src/app/components/table/book/book.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { MaterialInterface } from 'src/app/interfaces/material.interface';
-import { FilterTextService } from 'src/app/services/filter.service';
-import { actionUpdateData } from 'src/app/state/actions/list.actions';
+import { ListService } from 'src/app/services/list.service';
 import { selectListSuccess } from 'src/app/state/selectors/list.selectors';
 
 
@@ -41,7 +40,7 @@ export class BookComponent implements OnInit {
   constructor (
     public store: Store,
     private formBuilder: FormBuilder,
-    public service: FilterTextService
+    public listService: ListService
   ) {}
 
   ngOnInit(): void {
@@ -61,30 +60,6 @@ export class BookComponent implements OnInit {
   }
 
   book() {
-    const amount = this.formGroup.value.amount;
-    const req = indexedDB.open("MaterialsDatabase");
-    req.onsuccess = () => {
-      const db = req.result;
-      const transaction = db.transaction("materials", "readwrite")
-      const store = transaction.objectStore("materials");
-      const request = store.get(this.idMaterial)
-      request.onerror = () => {
-        alert()
-      };
-      request.onsuccess = () => {
-        const data = request.result;
-        data.Available = (+data.Available -amount).toString();
-        data.Quantity = (+data.Quantity +amount).toString();
-        const requestUpdate = store.put(data);
-        requestUpdate.onerror = () => {
-          alert('Book error')
-        };
-        requestUpdate.onsuccess = () =>{
-          this.store.dispatch(actionUpdateData({material:data, index:this.idMaterial-1}));
-          alert('Book success');
-          this.service.emitEvent()
-        };
-      };
-    }
+    this.listService.bookMaterial(this.formGroup.value.amount, this.idMaterial)
   }
 }
